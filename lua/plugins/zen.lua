1a5d25acc67f23b159286c37bfdcc65bb998cf0b
Michael Friderich <michael@frideri.ch>
2024-07-17 16:17:34 +0200

    Add zen plugin

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,9 @@
+-- Load zen (hhttps://github.com/folke/zen-mode.nvim)
+
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        opts = {}
+        vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', {})
+    end
+}
