0ff1926efc827addb514762f6495f6975fb17676
Michael Friderich <michael@frideri.ch>
2024-07-17 21:25:18 +0200

    Update none-ls plugin, refactor file

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,21 @@
 -- Load none-ls plugin (https://github.com/nvimtools/none-ls.nvim)
 
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.biome,
-        null_ls.builtins.formatting.black,    -- python
-        null_ls.builtins.formatting.codespell,
-        null_ls.builtins.formatting.isort,    -- python
-        null_ls.builtins.formatting.markdownlint, -- markdown
-        null_ls.builtins.formatting.prettier, -- javascript
-        null_ls.builtins.formatting.stylua,   -- lua
-        null_ls.builtins.diagnostics.erb_lint, -- javascript
-      },
-    })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.biome,
+				null_ls.builtins.formatting.black, -- python
+				null_ls.builtins.formatting.codespell,
+				null_ls.builtins.formatting.isort, -- python
+				null_ls.builtins.formatting.markdownlint, -- markdown
+				null_ls.builtins.formatting.prettier, -- javascript
+				null_ls.builtins.formatting.stylua, -- lua
+				null_ls.builtins.diagnostics.erb_lint, -- javascript
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
