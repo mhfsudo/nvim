d64ae3bbc1fa5501d1143ae1c070c9d441bd7f73
Michael Friderich <michael@frideri.ch>
2024-07-17 16:39:34 +0200

    Add alpha-nvim plugin

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,8 @@
+-- Load alpha plugin (https://github.com/goolord/alpha-nvim)
+
+return {
+    'goolord/alpha-nvim',
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.dashboard'.config)
+  end
+}
