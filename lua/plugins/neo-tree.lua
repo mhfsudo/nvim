be944b94ffcdfcac5634eb51a66bc8a014665105
Michael Friderich <michael@frideri.ch>
2024-07-17 21:26:24 +0200

    Update neo-tree plugin, add some options

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,7 +10,9 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      close_if_last_window = false,
       filesystem = {
+        hijack_netrw_behavior = "open_default",
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
@@ -20,5 +22,11 @@ return {
     vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>tc", ":Neotree close<CR>", {})
     vim.keymap.set("n", "<leader>tg", ":Neotree git_status float<CR>", {})
+    -- P toggles Preview
+    -- s open in vertical split
+    -- S open in horizontal split
+    -- a add file or directory/
+    -- d delete file or directory
+    -- r rename file or directory
   end,
 }
