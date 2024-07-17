505a48f109c16f79b51d585a66078381ea572e52
Michael Friderich <michael@frideri.ch>
2024-07-17 20:37:40 +0200

    Update neo-tree plugin, show dotfiles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,24 @@
 -- Load neo-tree (https://github.com/nvim-neo-tree/neo-tree.nvim)
 
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>tc", ":Neotree close<CR>", {})
-		vim.keymap.set("n", "<leader>tg", ":Neotree git_status float<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>tc", ":Neotree close<CR>", {})
+    vim.keymap.set("n", "<leader>tg", ":Neotree git_status float<CR>", {})
+  end,
 }
