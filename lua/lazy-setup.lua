58a64dcfa87810429c6b45f3b71e8c32f904e805
Michael Friderich <michael@frideri.ch>
2024-07-17 16:16:32 +0200

    Update init.lua, lazy.nvim, treesitter and telescope

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -7,5 +7,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.conceallevel = 2 -- required for Obsidian
 
+-- set leader key
 vim.g.mapleader = " "
+-- save with Control + s
 vim.keymap.set('n', '<C-s>', ':w<CR>', {})
+-- switch between windows with Tab
+vim.keymap.set('n', '<Tab>', '<C-w>w', {noremap = true, silent = true })
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,16 +1,16 @@
 -- Load colorscheme (https://github.com/catppuccin)
 
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    local config = require("catppuccin")
-      config.setup({
-        flavour = "macchiato", -- latte(white), frappe, macchiato, mocha
-        transparent_background = false
-    })
-    vim.cmd.colorscheme "catppuccin"
-  end
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        local config = require("catppuccin")
+        config.setup({
+            flavour = "macchiato", -- latte(white), frappe, macchiato, mocha
+            transparent_background = true
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end
 }
