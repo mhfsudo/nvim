c6ac06d3c0714bdf27265a7a5be04d1fbe10c514
Michael Friderich <michael@frideri.ch>
2024-07-19 17:18:42 +0200

    Update neo-tree plugin, change keymapping

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -30,7 +30,7 @@ return {
         }
       },
     })
-    vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>tt", ":Neotree filesystem reveal left<CR>", {})
     vim.keymap.set("n", "<leader>tc", ":Neotree close<CR>", {})
     vim.keymap.set("n", "<leader>tg", ":Neotree git_status float<CR>", {})
     -- P toggles Preview
