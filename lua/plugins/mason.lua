d35c300282f9886f4c2c4973ce7fa7d98612e7be
Michael Friderich <michael@frideri.ch>
2024-07-19 17:18:15 +0200

    Update mason plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,38 +11,81 @@ return {
 	{
 		-- (https://github.com/williamboman/mason-lspconfig.nvim)
 		"williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		-- (https://github.com/neovim/nvim-lspconfig)
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.bashls.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.dockerls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.groovyls.setup({})
-			lspconfig.html.setup({})
-			lspconfig.helm_ls.setup({})
-			lspconfig.jdtls.setup({})
-			lspconfig.biome.setup({})
-			lspconfig.jsonls.setup({})
-			lspconfig.kotlin_language_server.setup({})
-			lspconfig.ltex.setup({})
-			lspconfig.lua_ls.setup({})
-			lspconfig.markdown_oxide.setup({})
-			lspconfig.basedpyright.setup({})
-			lspconfig.terraformls.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.vimls.setup({})
-			lspconfig.yamlls.setup({})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.groovyls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.helm_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.biome.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.kotlin_language_server.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ltex.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.markdown_oxide.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.basedpyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.terraformls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.vimls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gdr", vim.lsp.buf.definition, {})
+			vim.keymap.set( "n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
